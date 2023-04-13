0ea41c7a906da6ac4eee4ca2f21830ef09cf887c
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-04-13 14:11:35 -0600

    attempt 7

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import { BrowserRouter, HashRouter, Router, Route } from 'react-router-dom';
+import { HashRouter, Router, Route } from 'react-router-dom';
 import Home from './Home';
 import LargePhoto from './LargePhoto';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <HashRouter>
-        <Router basename={`/`}>
-          <Route path="/" Component={Home} />
-          <Route path="/photo" Component={LargePhoto}></Route>
-        </Router>
-      </HashRouter>
-    </BrowserRouter>
+    <HashRouter>
+      <Router basename={`/`}>
+        <Route path="/" Component={Home} />
+        <Route path="/photo" Component={LargePhoto}></Route>
+      </Router>
+    </HashRouter>
   );
 };
 export default App;
