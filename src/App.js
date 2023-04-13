1dc6a43edbd9dbf92cdcff378ac6fb4d71b48a7e
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-04-13 14:20:47 -0600

    attempt 9

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Router, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import LargePhoto from './LargePhoto';
 
 const App = () => {
   return (
-    <>
-      <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
         <Route exact path="/" Component={Home} />
         <Route path="/photo" Component={LargePhoto}></Route>
-      </Router>
-    </>
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default App;
