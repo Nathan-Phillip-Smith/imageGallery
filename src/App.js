05cd50e6cd81017b2718ba7132b26473164c571f
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-04-13 13:56:37 -0600

    attempt 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import LargePhoto from './LargePhoto';
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route exact path="/imageGallery" Component={Home} />
-        <Route exact path="/imageGallery/photo" Component={LargePhoto}></Route>
-      </Routes>
+      <Router>
+        <Route path="/imageGallery" Component={Home} />
+        <Route path="/imageGallery/photo" Component={LargePhoto}></Route>
+      </Router>
     </>
   );
 };
